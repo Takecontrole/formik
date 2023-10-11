23ec383aefb64de520edc9963e9539c4d225d181
Takecontrole <takecontrole2034@gmail.com>
2023-10-11 15:19:34 +0400

    b

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useState } from "react";
-import BasicForm from "./components/BasicForm";
 import AdvancedForm from "./components/AdvancedForm";
 import "./App.css";
 
 function App() {
-  const [view, setView] = useState("basic");
+
   return (
     <div className="App">
       <nav>
         <h3
-          onClick={() => setView("basic")}
-          style={{ color: view === "basic" ? "#fff" : "" }}
+          
         >
-         Чистовик
+         formik
         </h3>
-        <h3
-          onClick={() => setView("advanced")}
-          style={{ color: view === "advanced" ? "#fff" : "" }}
-        >
-        Черновик
-        </h3>
+        
       </nav>
-      {view === "advanced" ? <BasicForm /> : <AdvancedForm />}
+      <AdvancedForm />
     </div>
   );
 }
